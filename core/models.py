8ee9108e71d31aaadf7017a498cf6c0867f57e7e
Taleh_M <taleh.i@labrin.tech>
2022-09-15 15:54:01 +0400

    add verbose name for core models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext as _
 from ckeditor.fields import RichTextField
 
 
@@ -8,71 +9,71 @@ from core.tools.base_models import Base_models
 # Create your models here.
 
 class Blog(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField(help_text=_('Enter the description of the blog'))
-    image = models.ImageField(upload_to='blog/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='blog/', blank=True, null=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.title
 
 
 class Service(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField()
-    image = models.ImageField(upload_to='service/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='service/', blank=True, null=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.title
 
 
 class Finance(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField()
-    image = models.ImageField(upload_to='finance/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='finance/', blank=True, null=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.title
 
 
 class Tax(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField()
-    image = models.ImageField(upload_to='tax/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='tax/', blank=True, null=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.title
 
 
 class HR(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField()
-    image = models.ImageField(upload_to='hr/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='hr/', blank=True, null=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.title
-from django.utils.translation import gettext as _
+
 
 class About(Base_models):
-    title = models.CharField(max_length=200)
-    description = RichTextField()
-    image = models.ImageField(upload_to='about/', blank=True, null=True)
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = RichTextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='about/', blank=True, null=True, verbose_name=_('Image'))
     
     def __str__(self):
         return self.title
 
 
 class Contact(Base_models):
-    name = models.CharField(max_length=200)
-    number = models.CharField(max_length=200)
-    subject = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name=_('Name'))
+    number = models.CharField(max_length=200, verbose_name=_('Number'))
+    subject = models.CharField(max_length=200, verbose_name=_('Subject'))
 
     def __str__(self):
         return self.name
 
 
 class FAQ(Base_models):
-    question = models.CharField(max_length=200)
-    answer = models.TextField(max_length=10000)
+    question = models.CharField(max_length=200, verbose_name=_('Question'))
+    answer = models.TextField(max_length=10000, verbose_name=_('Answer'))
 
     def __str__(self):
         return self.question
