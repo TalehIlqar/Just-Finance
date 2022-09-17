82592579020369be32b3ffa40672a199b2c16af4
vusallyv <vusal581176@gmail.com>
2022-09-17 23:39:27 +0400

    dynamic values

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
+from core.models import About, HR, FAQ, Service, Blog
+
+
 # Create your views here.
 
 
@@ -8,23 +11,34 @@ def index(request):
     context = {
         "title": _("Home"),
     }
-    return render(request, 'index.html', context)
+    return render(request, "index.html", context)
 
 
 def about(request):
     context = {
         "title": _("About"),
+        "about": About.objects.last(),
+        "hr": HR.objects.last(),
+        "faqs": FAQ.objects.all(),
     }
-    return render(request, 'about.html')
+    return render(request, "about.html", context)
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
 
 
 def services(request):
-    return render(request, 'service.html')
+    context = {
+        "title": _("Services"),
+        "services": Service.objects.all(),
+    }
+    return render(request, "service.html", context)
 
 
 def blog(request):
-    return render(request, 'blog.html')
+    context = {
+        "title": _("Blog"),
+        "blogs": Blog.objects.all(),
+    }
+    return render(request, "blog.html", context)
--- just_finance/urls.py
+++ just_finance/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('metal/', admin.site.urls),
@@ -23,3 +24,8 @@ urlpatterns = [
     path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
