d05c79a8d58a3161719eb999b1487b2b3a43baea
Taleh_M <taleh.i@labrin.tech>
2022-09-20 13:08:55 +0400

    -

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# path('subscriberapi/', views.SubscriberAPIView.as_view(), name='subscriberapi'),
+path('subscriberapi/', views.SubscriberAPIView.as_view(), name='subscriberapi'),
--- just_finance/urls.py
+++ just_finance/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from core.api.routers import api
 
 urlpatterns = [
     path('metal/', admin.site.urls),
