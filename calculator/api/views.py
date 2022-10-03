2ca09d18097c14731572751e75b786bb850018a5
vusal <abdulla.vusal.3@gmail.com>
2022-10-03 12:28:35 +0400

    evalx

--- calculator/api/views.py
+++ calculator/api/views.py
@@ -36,7 +36,7 @@ class CalculatorView(APIView):
                                     "fee": {
                                             "from_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().from_number,
                                             "to_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().to_number,
-                                            "fee": eval(insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().calculate_formula, {}, {"x": 1}),
+                                            "fee": eval(insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().calculate_formula, {}, {"x": from_number}),
                                         },
                                 }
                                 for insurance_type in tax_type.insurance_types.all()
