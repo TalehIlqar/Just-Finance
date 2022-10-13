54ab5bfcf55caa272377c1afc3f6a43e7a701699
vusal <abdulla.vusal.3@gmail.com>
2022-10-13 20:04:31 +0400

    calculator

--- calculator/api/views.py
+++ calculator/api/views.py
@@ -29,15 +29,15 @@ class CalculatorView(APIView):
                     "tax_types": [
                         {
                             "id": tax_type.id,
-                            "name": tax_type.name.name,
+                            "name": tax_type.name,
                             "insurance_types": [
                                 {
                                     "id": insurance_type.id,
-                                    "name": insurance_type.name.name,
+                                    "name": insurance_type.name,
                                     "fee": {
-                                            "from_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().from_number,
-                                            "to_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().to_number,
-                                            "fee": eval(insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().from_to_formula, {}, {"x": from_number}),
+                                            "from_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().from_number if insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first() else None,
+                                            "to_number": insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().to_number if insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first() else None,
+                                            "fee": eval(insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first().from_to_formula, {}, {"x": from_number}) if insurance_type.fees.filter(from_number__lte=from_number).order_by("-from_number").first() else None,
                                         },
                                 }
                                 for insurance_type in tax_type.insurance_types.all()
