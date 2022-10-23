6b4b178d9acb6e30cd4944b73b91cd73e341d0f6
vusal <abdulla.vusal.3@gmail.com>
2022-10-23 19:50:31 +0400

    net to gross

--- calculator/api/views.py
+++ calculator/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
 from rest_framework import permissions, status
 from rest_framework.response import Response
@@ -9,69 +10,37 @@ from calculator.models import SectorType
 class CalculatorView(APIView):
     permission_classes = [permissions.AllowAny]
 
-    def post(self, request, format=None):
-        try:
-            sector_type = SectorType.objects.get(id=request.data["sector_type"])
-        except SectorType.DoesNotExist:
-            return Response(
-                {"error": _("Sector type does not exist.")},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    def post(self, request):
+        sector_type = get_object_or_404(SectorType, id=request.data.get("sector_type"))
         from_number = request.data.get("from_number")
         to_number = request.data.get("to_number")
 
         if from_number:
             qs_filter = {
                 "from_number__lte": from_number,
-                "to_number__gte": from_number,
+                "to_number__gt": from_number,
                 "from_to_formula__isnull": False,
             }
         elif to_number:
             qs_filter = {
                 "from_number__lte": to_number,
-                "to_number__gte": to_number,
+                "to_number__gt": to_number,
                 "to_from_formula__isnull": False,
             }
-
-        return Response(
-            {
-                "sector_type": {
-                    "id": sector_type.id,
-                    "name": sector_type.name,
-                    "tax_types": [
+        else:
+            return Response(
+                {"error": _("Please provide from_number or to_number")},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        data = []
+        for tax_type in sector_type.tax_types.all():
+            for insurance_type in tax_type.insurance_types.all():
+                insurance_fees = insurance_type.fees.filter(**qs_filter).order_by("insurance_type__name")
+                for insurance_fee in insurance_fees:
+                    data.append(
                         {
-                            "id": tax_type.id,
-                            "name": tax_type.name,
-                            "insurance_types": [
-                                {
-                                    "id": insurance_type.id,
-                                    "name": insurance_type.name,
-                                    "fee": {
-                                        "from_number": insurance_type.fees.filter(
-                                            **qs_filter).order_by(
-                                            "insurance_type__name").first().from_number if insurance_type.fees.filter(
-                                            **qs_filter).order_by("insurance_type__name").first() else None,
-                                        "to_number": insurance_type.fees.filter(**qs_filter).order_by(
-                                            "insurance_type__name").first().to_number if insurance_type.fees.filter(
-                                            **qs_filter).order_by("insurance_type__name").first() else None,
-                                        "fee": round(eval(
-                                            insurance_type.fees.filter(**qs_filter).order_by(
-                                                "insurance_type__name").first().from_to_formula
-                                            if from_number else insurance_type.fees.filter(
-                                                **qs_filter).order_by(
-                                                "insurance_type__name").first().to_from_formula, {},
-                                            {"x": from_number if from_number else to_number}),
-                                            2) if insurance_type.fees.filter(
-                                            **qs_filter).order_by(
-                                            "insurance_type__name").first() else None,
-
-                                    },
-                                }
-                                for insurance_type in tax_type.insurance_types.all()
-                            ],
+                            "tax_type": tax_type.name,
+                            "fee": round(eval(insurance_fee.from_to_formula or insurance_fee.to_from_formula, {}, {'x': from_number or to_number})),
                         }
-                        for tax_type in sector_type.tax_types.all()
-                    ],
-                }
-            }
-        )
+                    )
+        return Response(data, status=status.HTTP_200_OK)
