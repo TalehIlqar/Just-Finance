2627b0759cc6c7d60a600eb29e98656e5a411b47
Taleh_M <taleh.i@labrin.tech>
2022-10-23 12:46:41 +0400

    fix csrf token

--- just_finance/settings.py
+++ just_finance/settings.py
@@ -169,4 +169,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://justfinance.az",
+    "https://127.0.0.1",
+]
