76f3d3983784d5e7d03492bf1f1422c6faf8ce0e
vusal <abdulla.vusal.3@gmail.com>
2022-10-23 19:50:31 +0400

    net to gross

--- calculator/views.py
+++ calculator/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
-from calculator.models import SectorType
+from calculator.models import SectorType, InsuranceType
 
 
 # Create your views here.
@@ -11,5 +11,6 @@ def calculator(request):
     context = {
         "title": _("Calculator"),
         "sector_types": SectorType.objects.all(),
+        "insurance_type_names": [insurance_type[0] for insurance_type in sorted(InsuranceType.NAME_CHOICES, key=lambda x: x[1])],
     }
     return render(request, "pages/calculator.html", context)
